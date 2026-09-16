cebc6fe4a08a4d956d77a738cdc181db73fc9380
Shahbaz181 <shahbaz1861915@gmail.com>
2026-09-16 16:22:13 +0530

    practice problems

---
+++ Practice-Problems/practice 6/Best_time_to_buy_and_sell_stocks.java
@@ -0,0 +1,21 @@
+public class Best_time_to_buy_and_sell_stocks {
+    public static int buyAndsellStocks(int prices[]) {
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyprice < prices[i]) { // profit
+                int profit = prices[i] - buyprice; // today's profit
+                maxprofit = Math.max(maxprofit, profit);
+            } else {
+                buyprice = prices[i];
+            }
+        }
+        return maxprofit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(buyAndsellStocks(prices));
+    }
+}
